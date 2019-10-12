0d483e4ba5eba8fd31325c5503a034b48d3177e9
Prarthna Sabharwal <prarthna.sl@gmail.com>
2019-10-12 13:28:16 +0530

    Adding contract for ERC20 Token sale

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.5.8;
 import "./ERC20Token.sol";
 
 contract TokenSale {
-    address admin;
+    address payable manager;
     ERC20Token public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
 
     event Sell(address _buyer, uint256 _amount);
+    event EndSale(uint256 _totalAmountSold);
 
-    function TokenSale(ERC20Token _tokenContract, uint256 _tokenPrice) public {
-        admin = msg.sender;
+    constructor(ERC20Token _tokenContract, uint256 _tokenPrice) public {
+        manager = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
@@ -22,19 +23,21 @@ contract TokenSale {
 
     function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens); // Contract should have requested number of tokens
         require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
         tokensSold += _numberOfTokens;
 
-        Sell(msg.sender, _numberOfTokens);
+        emit Sell(msg.sender, _numberOfTokens);
     }
 
     function endSale() public {
-        require(msg.sender == admin);
-        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
+        require(msg.sender == manager);
+        require(tokenContract.transfer(manager, tokenContract.balanceOf(address(this))));
 
         // Transfering the balance to the manager before ending sale
-        admin.transfer(address(this).balance);
+        manager.transfer(address(this).balance);
+         emit EndSale(tokensSold);
+        /* selfdestruct(manager); */
     }
 }
