aa8abb0851073e09536274e01909df1b25d78df0
Prarthna Sabharwal <prarthna.sl@gmail.com>
2019-10-12 13:27:20 +0530

    Migrations for contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var ERC20Token = artifacts.require("./ERC20Token.sol");
+var TokenSale = artifacts.require("./TokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC20Token);
+  deployer.deploy(ERC20Token, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(TokenSale, ERC20Token.address, tokenPrice);
+  });
 };
